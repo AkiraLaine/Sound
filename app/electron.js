2e824b62dbc49e7b014103cac585e5fe89c0762c
Akira Laine <akira_laine@outlook.com>
2016-06-29 21:38:12 +1100

    updated electron.js

--- app/electron.js
+++ app/electron.js
@@ -2,8 +2,52 @@
 
 const electron = require('electron')
 const path = require('path')
+const fs = require('fs')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
+
+//Lets require/import the HTTP module
+var http = require('http');
+
+//Lets define a port we want to listen to
+const PORT=52308; 
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+  if (request.url === "/") {
+    var filePath = path.join(__dirname, '/dist/index.html');
+    var stat = fs.statSync(filePath);
+
+    response.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Content-Length': stat.size
+    });
+
+    var readStream = fs.createReadStream(filePath);
+    // We replaced all the event handlers with a simple call to readStream.pipe()
+    readStream.pipe(response);
+  }
+
+  if(request.url === "/build.js") {
+    var filePath = path.join(__dirname, '/dist/build.js');
+    var stat = fs.statSync(filePath);
+
+    response.writeHead(200, {
+        'Content-Type': 'text/javascript',
+        'Content-Length': stat.size
+    });
+
+    var readStream = fs.createReadStream(filePath);
+    // We replaced all the event handlers with a simple call to readStream.pipe()
+    readStream.pipe(response);
+  }
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT);
 
 let mainWindow
 let config = {}
@@ -27,7 +71,7 @@ function createWindow () {
     resizable: false
   })
 
-  mainWindow.loadURL(config.url)
+  mainWindow.loadURL("http://localhost:52308")
   if (process.env.NODE_ENV === 'development') mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', () => {
